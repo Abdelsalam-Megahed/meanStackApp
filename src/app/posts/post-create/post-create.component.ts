cf73d56f6db600359321ca444ad941d48e3071df
Mac <mac@Macs-MacBook-Pro.local>
2019-04-13 16:49:13 +0200

    Edit button now works!
    Edit button functionality is added, also posts is updated from the server.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -9,17 +11,39 @@ import { PostsService } from '../posts.service';
 })
 export class PostCreateComponent implements OnInit {
 
-  constructor(public postsService: PostsService) { }
+   mode = 'create';
+   postId: string;
+   post: Post;
+
+  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+        if(paramMap.has('postId')){
+            this.mode = 'edit';
+            this.postId = paramMap.get('postId');
+            this.postsService.getPost(this.postId).subscribe(postData => {
+            this.post = {id: postData._id, title: postData.title, content: postData.content};
+            });
+        }else{
+          this.mode = 'create';
+          this.postId = null;
+        }
+    });
   }
 
-  onAddPost(form: NgForm){
+  onSavePost(form: NgForm){
     if(form.invalid){
       return;
     }
+    if(this.mode === 'create'){
       this.postsService.addPost(form.value.title,form.value.content);
+
+    }else{
+      this.postsService.updatePost(this.postId,form.value.title,form.value.content);
+    }
+    form.resetForm();
     }
 
  }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -31,6 +31,10 @@ export class PostsService {
     // return [...this.posts];
   }
 
+  getPost(id: string){
+      return this.http.get<{_id: string, title: string, content:string}>('http://localhost:3000/api/posts/' + id);
+  }
+
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
@@ -45,6 +49,21 @@ export class PostsService {
         post.id = postId;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+    });
+  }
+
+  updatePost(id: string, title: string, content: string){
+    const post: Post = {id: id ,title: title, content: content};
+    this.http
+    .put("http://localhost:3000/api/posts/" + id, post)
+    .subscribe(response => {
+      console.log(response);
+
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
     });
   }
 
