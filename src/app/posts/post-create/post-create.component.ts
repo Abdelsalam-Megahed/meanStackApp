93ad5d6fae5c5aed693b506eb66fb0596886547c
Mac <mac@Macs-MacBook-Pro.local>
2019-04-14 14:58:49 +0200

    Adding an image preview.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
    postId: string;
    post: Post;
    isLoading = false;
+   imagePreview: string;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
@@ -27,6 +28,9 @@ export class PostCreateComponent implements OnInit {
           }),
           'content': new FormControl(null,{
             validators: [Validators.required, Validators.minLength(3)]
+          }),
+          'image': new FormControl(null, {
+            validators: [Validators.required]
           })
 
       });
@@ -47,6 +51,19 @@ export class PostCreateComponent implements OnInit {
           this.postId = null;
         }
     });
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
+    const reader = new FileReader();
+    reader.onload = ( ) => {
+          this.imagePreview = reader.result;
+      };
+    reader.readAsDataURL(file);
   }
 
   onSavePost(){
