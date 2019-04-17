d853810f1414265130c74917daa3f676595cf302
Mac <mac@Macs-MacBook-Pro.local>
2019-04-17 14:25:27 +0200

    Pagination is done.
    now the paginator is only shopwing up if there's a post and posts are counted.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -14,29 +14,42 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   private postsSub: Subscription;
-  totalPosts = 10;
+  totalPosts = 0;
   postPerPage = 2;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
+  isLoading = false;
+
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
-     this.postsService.getPosts(this.postPerPage, this.currentPage);
-     this.postsSub = this.postsService.getPostUpdateListener()
-      .subscribe((posts: Post[]) => {
-        this.posts = posts;
+    this.isLoading = true;
+
+    this.postsService.getPosts(this.postPerPage, this.currentPage);
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
+        this.isLoading = false;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
       });
   }
 
   onChangePage(pageData: PageEvent) {
-      this.currentPage = pageData.pageIndex + 1;
-      this.postPerPage = pageData.pageSize;
-      this.postsService.getPosts(this.postPerPage, this.currentPage);
+        this.isLoading = true;
+
+        this.currentPage = pageData.pageIndex + 1;
+        this.postPerPage = pageData.pageSize;
+        this.postsService.getPosts(this.postPerPage, this.currentPage);
   }
 
   onDelete(postId: string) {
-      this.postsService.deletePost(postId);
+      this.isLoading = true;
+
+      this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postPerPage, this.currentPage);
+      });
+
   }
 
   ngOnDestroy() {
