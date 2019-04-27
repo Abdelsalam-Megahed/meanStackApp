d012d39c30fb3cd9da3d853e1950fab4d7e0669b
Mac <mac@Macs-MacBook-Pro.local>
2019-04-27 22:32:05 +0200

    Improving the UI to reflect the authentication status
    In the header this is shown clearly.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 
 export class AuthService{
+  private token: string;
+  private authStatusListener = new Subject<Boolean>();
+
   constructor(private http: HttpClient) {}
+
+  getToken(){
+    return this.token;
+  }
+
+  getAuthStatusListener(){
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string){
 
@@ -21,6 +33,9 @@ export class AuthService{
     this.http.post('http://localhost:3000/api/user/login', authData)
     .subscribe(response => {
       console.log(response);
+      const token = response.token;
+      this.token = token;
+      this.authStatusListener.next(true);
     });
   }
 }
