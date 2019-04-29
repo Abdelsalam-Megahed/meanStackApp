224cbd7a6cd793b1672d27876b1176c25a54db4b
Mac <mac@Macs-MacBook-Pro.local>
2019-04-29 02:04:06 +0200

    Some error handling for login and logout page.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
+
+import { AuthService } from "../auth.service";
 
 @Component({
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
-
-export class SignupComponent{
+export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private authStatusSub: Subscription;
 
-  constructor( public authService: AuthService){}
+  constructor(public authService: AuthService) {}
 
-  onsignup( form: NgForm){
-    if(form.invalid){
+  ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  onSignup(form: NgForm) {
+    if (form.invalid) {
       return;
     }
+    this.isLoading = true;
     this.authService.createUser(form.value.email, form.value.password);
-    this.isLoading = false;
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
